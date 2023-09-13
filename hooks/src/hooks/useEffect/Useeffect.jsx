30c5598347524678fa67ab73dec057db5759f0d3
furqansup <furqanahmedmj@gmail.com>
2023-09-14 00:00:04 +0530

    useEffect

--- hooks/src/hooks/useEffect/Useeffect.jsx
+++ hooks/src/hooks/useEffect/Useeffect.jsx
@@ -1,21 +1,33 @@
 import React, { useState, useEffect } from "react";
 
 function Useeffect() {
+  const [time, setTime] = useState(0);
   const [count, setCount] = useState(0);
 
   useEffect(() => {
     const interValid = setInterval(() => {
-      setCount(count + 1);
+      setTime(time + 1);
     }, 1000);
     return () => {
       clearInterval(interValid);
     };
-  }, [count]);
+  }, [time]);
+
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
 
   return (
     <>
       <h1 style={{ textAlign: "center" }}>UseEffect Hook</h1>
+      <p style={{ textAlign: "center" }}>Timer : {time}</p>
       <p style={{ textAlign: "center" }}>Count : {count}</p>
+      <button onClick={handleAdd}>Add</button>
+      <button onClick={handleReset}>Reset</button>
     </>
   );
 }
